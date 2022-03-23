178e9efb4df846f42358e62de4ba947f12fb4b0f
DRIESSEB <bart.driessen@vito.be>
2022-03-23 20:03:47 +0100

    Switch to aggregate_spatial

--- src/openeo_classification/main.py
+++ src/openeo_classification/main.py
@@ -45,7 +45,7 @@ if first_time:
 else:
     crop_ids, other_crop_ids = sample.get_crop_codes()
 
-crops_of_interest = False
+crops_of_interest = True
 if crops_of_interest:
     ids = crop_ids
     fp = base_path / "crops_of_interest"
@@ -57,14 +57,14 @@ else:
 def run_jobs(df, fnp, features, year, output_job_stat_path):
     while True:
         if len(df[(df["status"]=="running") | (df["status"]=="queued")]) < 2:
-            with open(fnp) as fn:
-                pol = fn.readlines()[0]
-            sampled_features = features.filter_spatial(json.loads(pol))
+            pols = gpd.read_file(fnp,crs=4326)
+            pols["geometry"] = pols["geometry"].centroid
+            sampled_features = features.aggregate_spatial(json.loads(pols.to_json()), reducer="mean")
             job = sampled_features.send_job(
                 title="Punten extraheren van sentinelhub",
-                out_format="netCDF",
-                sample_by_feature=True,
-                job_options=job_options)
+                out_format="CSV",
+                job_options=job_options,
+            )
             job.start_job()
             print("Job ID: {}; Year: {}; Crop ID: {}".format(job.job_id, year, fnp))
             df = df.append({
@@ -84,16 +84,79 @@ def run_jobs(df, fnp, features, year, output_job_stat_path):
             df.to_csv(output_job_stat_path,index=False)
 
 
+corrupt_jsons = [
+    r"c:\users\driesseb\dev\openeo-classification\src\openeo_classification\resources\training_data\crops_of_interest\sentinelhub\sampleable_polygons_year2018_zone39_id5100_p0.json",
+    r"c:\users\driesseb\dev\openeo-classification\src\openeo_classification\resources\training_data\other_crops\sentinelhub\sampleable_polygons_year2017_zone31_id4351_p0.json",
+    r"c:\users\driesseb\dev\openeo-classification\src\openeo_classification\resources\training_data\other_crops\sentinelhub\sampleable_polygons_year2017_zone31_id4351_p1.json",
+    r"c:\users\driesseb\dev\openeo-classification\src\openeo_classification\resources\training_data\other_crops\sentinelhub\sampleable_polygons_year2017_zone32_id2900_p0.json",
+    r"c:\users\driesseb\dev\openeo-classification\src\openeo_classification\resources\training_data\other_crops\sentinelhub\sampleable_polygons_year2017_zone33_id1120_p0.json",
+    r"c:\users\driesseb\dev\openeo-classification\src\openeo_classification\resources\training_data\other_crops\sentinelhub\sampleable_polygons_year2017_zone33_id1700_p0.json",
+    r"c:\users\driesseb\dev\openeo-classification\src\openeo_classification\resources\training_data\other_crops\sentinelhub\sampleable_polygons_year2017_zone33_id1900_p0.json",
+    r"c:\users\driesseb\dev\openeo-classification\src\openeo_classification\resources\training_data\other_crops\sentinelhub\sampleable_polygons_year2017_zone33_id2260_p0.json",
+    r"c:\users\driesseb\dev\openeo-classification\src\openeo_classification\resources\training_data\other_crops\sentinelhub\sampleable_polygons_year2017_zone33_id2900_p0.json",
+    r"c:\users\driesseb\dev\openeo-classification\src\openeo_classification\resources\training_data\other_crops\sentinelhub\sampleable_polygons_year2017_zone33_id4100_p0.json",
+    r"c:\users\driesseb\dev\openeo-classification\src\openeo_classification\resources\training_data\other_crops\sentinelhub\sampleable_polygons_year2017_zone33_id4351_p0.json",
+    r"c:\users\driesseb\dev\openeo-classification\src\openeo_classification\resources\training_data\other_crops\sentinelhub\sampleable_polygons_year2017_zone33_id4380_p0.json",
+    r"c:\users\driesseb\dev\openeo-classification\src\openeo_classification\resources\training_data\other_crops\sentinelhub\sampleable_polygons_year2017_zone33_id7700_p0.json",
+    r"c:\users\driesseb\dev\openeo-classification\src\openeo_classification\resources\training_data\other_crops\sentinelhub\sampleable_polygons_year2017_zone33_id7900_p0.json",
+    r"c:\users\driesseb\dev\openeo-classification\src\openeo_classification\resources\training_data\other_crops\sentinelhub\sampleable_polygons_year2017_zone33_id9100_p0.json",
+    r"c:\users\driesseb\dev\openeo-classification\src\openeo_classification\resources\training_data\other_crops\sentinelhub\sampleable_polygons_year2017_zone33_id9100_p1.json",
+    r"c:\users\driesseb\dev\openeo-classification\src\openeo_classification\resources\training_data\other_crops\sentinelhub\sampleable_polygons_year2017_zone33_id9100_p2.json",
+    r"c:\users\driesseb\dev\openeo-classification\src\openeo_classification\resources\training_data\other_crops\sentinelhub\sampleable_polygons_year2017_zone33_id9110_p0.json",
+    r"c:\users\driesseb\dev\openeo-classification\src\openeo_classification\resources\training_data\other_crops\sentinelhub\sampleable_polygons_year2017_zone33_id9110_p1.json",
+    r"c:\users\driesseb\dev\openeo-classification\src\openeo_classification\resources\training_data\other_crops\sentinelhub\sampleable_polygons_year2017_zone33_id9120_p0.json",
+    r"c:\users\driesseb\dev\openeo-classification\src\openeo_classification\resources\training_data\other_crops\sentinelhub\sampleable_polygons_year2018_zone31_id9213_p0.json",
+    r"c:\users\driesseb\dev\openeo-classification\src\openeo_classification\resources\training_data\other_crops\sentinelhub\sampleable_polygons_year2018_zone37_id0_p0.json",
+    r"c:\users\driesseb\dev\openeo-classification\src\openeo_classification\resources\training_data\other_crops\sentinelhub\sampleable_polygons_year2018_zone37_id0_p1.json",
+    r"c:\users\driesseb\dev\openeo-classification\src\openeo_classification\resources\training_data\other_crops\sentinelhub\sampleable_polygons_year2018_zone37_id1100_p0.json",
+    r"c:\users\driesseb\dev\openeo-classification\src\openeo_classification\resources\training_data\other_crops\sentinelhub\sampleable_polygons_year2018_zone37_id1500_p0.json",
+    r"c:\users\driesseb\dev\openeo-classification\src\openeo_classification\resources\training_data\other_crops\sentinelhub\sampleable_polygons_year2018_zone37_id1500_p1.json",
+    r"c:\users\driesseb\dev\openeo-classification\src\openeo_classification\resources\training_data\other_crops\sentinelhub\sampleable_polygons_year2018_zone37_id1700_p0.json",
+    r"c:\users\driesseb\dev\openeo-classification\src\openeo_classification\resources\training_data\other_crops\sentinelhub\sampleable_polygons_year2018_zone37_id1700_p1.json",
+    r"c:\users\driesseb\dev\openeo-classification\src\openeo_classification\resources\training_data\other_crops\sentinelhub\sampleable_polygons_year2018_zone37_id1900_p0.json",
+    r"c:\users\driesseb\dev\openeo-classification\src\openeo_classification\resources\training_data\other_crops\sentinelhub\sampleable_polygons_year2018_zone37_id1900_p1.json",
+    r"c:\users\driesseb\dev\openeo-classification\src\openeo_classification\resources\training_data\other_crops\sentinelhub\sampleable_polygons_year2018_zone37_id2230_p0.json",
+    r"c:\users\driesseb\dev\openeo-classification\src\openeo_classification\resources\training_data\other_crops\sentinelhub\sampleable_polygons_year2018_zone37_id2900_p0.json",
+    r"c:\users\driesseb\dev\openeo-classification\src\openeo_classification\resources\training_data\other_crops\sentinelhub\sampleable_polygons_year2018_zone37_id3300_p0.json",
+    r"c:\users\driesseb\dev\openeo-classification\src\openeo_classification\resources\training_data\other_crops\sentinelhub\sampleable_polygons_year2018_zone37_id3300_p1.json",
+    r"c:\users\driesseb\dev\openeo-classification\src\openeo_classification\resources\training_data\other_crops\sentinelhub\sampleable_polygons_year2018_zone37_id3490_p0.json",
+    r"c:\users\driesseb\dev\openeo-classification\src\openeo_classification\resources\training_data\other_crops\sentinelhub\sampleable_polygons_year2018_zone37_id3690_p0.json",
+    r"c:\users\driesseb\dev\openeo-classification\src\openeo_classification\resources\training_data\other_crops\sentinelhub\sampleable_polygons_year2018_zone37_id3690_p1.json",
+    r"c:\users\driesseb\dev\openeo-classification\src\openeo_classification\resources\training_data\other_crops\sentinelhub\sampleable_polygons_year2018_zone37_id4380_p0.json",
+    r"c:\users\driesseb\dev\openeo-classification\src\openeo_classification\resources\training_data\other_crops\sentinelhub\sampleable_polygons_year2018_zone37_id4380_p1.json",
+    r"c:\users\driesseb\dev\openeo-classification\src\openeo_classification\resources\training_data\other_crops\sentinelhub\sampleable_polygons_year2018_zone37_id4420_p0.json",
+    r"c:\users\driesseb\dev\openeo-classification\src\openeo_classification\resources\training_data\other_crops\sentinelhub\sampleable_polygons_year2018_zone37_id4420_p1.json",
+    r"c:\users\driesseb\dev\openeo-classification\src\openeo_classification\resources\training_data\other_crops\sentinelhub\sampleable_polygons_year2018_zone37_id4420_p2.json",
+    r"c:\users\driesseb\dev\openeo-classification\src\openeo_classification\resources\training_data\other_crops\sentinelhub\sampleable_polygons_year2018_zone37_id4420_p3.json",
+    r"c:\users\driesseb\dev\openeo-classification\src\openeo_classification\resources\training_data\other_crops\sentinelhub\sampleable_polygons_year2018_zone37_id4490_p0.json",
+    r"c:\users\driesseb\dev\openeo-classification\src\openeo_classification\resources\training_data\other_crops\sentinelhub\sampleable_polygons_year2018_zone37_id5000_p0.json",
+    r"c:\users\driesseb\dev\openeo-classification\src\openeo_classification\resources\training_data\other_crops\sentinelhub\sampleable_polygons_year2018_zone37_id7900_p0.json",
+    r"c:\users\driesseb\dev\openeo-classification\src\openeo_classification\resources\training_data\other_crops\sentinelhub\sampleable_polygons_year2018_zone37_id9100_p0.json",
+    r"c:\users\driesseb\dev\openeo-classification\src\openeo_classification\resources\training_data\other_crops\sentinelhub\sampleable_polygons_year2018_zone37_id9100_p1.json",
+    r"c:\users\driesseb\dev\openeo-classification\src\openeo_classification\resources\training_data\other_crops\sentinelhub\sampleable_polygons_year2018_zone37_id9110_p0.json",
+    r"c:\users\driesseb\dev\openeo-classification\src\openeo_classification\resources\training_data\other_crops\sentinelhub\sampleable_polygons_year2018_zone37_id9211_p0.json",
+    r"c:\users\driesseb\dev\openeo-classification\src\openeo_classification\resources\training_data\other_crops\sentinelhub\sampleable_polygons_year2018_zone37_id9998_p0.json",
+    r"c:\users\driesseb\dev\openeo-classification\src\openeo_classification\resources\training_data\other_crops\sentinelhub\sampleable_polygons_year2018_zone38_id0_p0.json",
+    r"c:\users\driesseb\dev\openeo-classification\src\openeo_classification\resources\training_data\other_crops\sentinelhub\sampleable_polygons_year2018_zone38_id0_p1.json",
+    r"c:\users\driesseb\dev\openeo-classification\src\openeo_classification\resources\training_data\other_crops\sentinelhub\sampleable_polygons_year2018_zone38_id0_p2.json",
+    r"c:\users\driesseb\dev\openeo-classification\src\openeo_classification\resources\training_data\other_crops\sentinelhub\sampleable_polygons_year2018_zone38_id1100_p0.json",
+    r"c:\users\driesseb\dev\openeo-classification\src\openeo_classification\resources\training_data\other_crops\sentinelhub\sampleable_polygons_year2018_zone38_id1100_p1.json",
+    r"c:\users\driesseb\dev\openeo-classification\src\openeo_classification\resources\training_data\other_crops\sentinelhub\sampleable_polygons_year2018_zone38_id1100_p2.json"
+]
 
-for year in years:
+
+for year in [2018, 2019]:#years:
     for prov in ["terrascope", "sentinelhub"]:
         for zone in zones:
             features = load_features(year, connection_provider = connection, provider = prov, sampling=True)
             for fnp in glob.glob(str(fp / prov / ("*"+str(year)+"_zone"+str(zone)+"*"))):
-                with open(fnp) as fn:
-                    pol = fn.readlines()[0]
-                    df = create_or_load_job_statistics(path = base_path / "job_statistics.csv")
-                    if not df.empty and fnp in df["fp"].tolist():
-                        continue
-                    df = run_jobs(df, fnp, features, year, base_path / "job_statistics.csv")
+                if fnp in corrupt_jsons:
+                    continue
+                else:
+                    with open(fnp) as fn:
+                        pol = fn.readlines()[0]
+                        df = create_or_load_job_statistics(path = base_path / "job_statistics.csv")
+                        if not df.empty and fnp in df["fp"].tolist():
+                            continue
+                        df = run_jobs(df, fnp, features, year, base_path / "job_statistics.csv")
 
