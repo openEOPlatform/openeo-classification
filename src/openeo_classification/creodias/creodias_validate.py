c54dc9aacca502d8acd7027147bcef46013a7004
Jeroen Dries <jeroen.dries@vito.be>
2022-06-22 07:59:46 +0200

    add some outstanding changes, e.g. to avoid rate limiting errors

--- src/openeo_classification/creodias/creodias_validate.py
+++ src/openeo_classification/creodias/creodias_validate.py
@@ -1,4 +1,6 @@
 import json
+from time import sleep
+from datetime import timedelta, date
 
 from shapely.geometry import mapping
 
@@ -6,23 +8,30 @@ from openeo.rest import OpenEoApiError
 from openeo_classification.connection import creo, terrascope_dev
 from openeo_classification.features import sentinel2_features
 from openeo_classification.grids import UTM_100km_EU27, UTM_100km_World
-tiles = UTM_100km_World()
+tiles = UTM_100km_EU27()
 
 result = []
 
-#with open("result_2019.json", "r+") as f:
+#with open("result_2017.json", "r+") as f:
 #    data = json.load(f)
 #    result = data["features"]
 previously_loaded = {f["id"]:f for f in result}
 
-idx_dekad, idx_list, s2_list  = sentinel2_features(2021,creo,provider="creodias")
+year = 2017
+start_date = date(year,1,1)
+end_date = date(year,12,31)
+
+idx_dekad  = sentinel2_features(start_date,end_date,creo,provider="creodias")
 for idx,row in tiles.iterrows():
     box = row.geometry.bounds
     tileID = row['name'][:5]
     print(tileID)
     if tileID not in previously_loaded:
         try:
-            errors = idx_dekad.filter_bbox(west=box[0], south=box[1], east=box[2], north=box[3]).validate()
+            sleep(1)
+            print(box)
+            buffer = 0.2
+            errors = idx_dekad.filter_bbox(west=box[0] + buffer, south=box[1] + buffer, east=box[2] - buffer, north=box[3] - buffer).validate()
             tiles = list(set([ msg['message'][6:66] for msg in errors if tileID in msg['message'] ]))
             print(len(tiles))
             print(tiles)
