125ef6dd05fef875579d2a04988b3d507e6dfe2d
Jeroen Dries <jeroen.dries@vito.be>
2022-07-18 08:07:56 +0200

    example scripts to sample lucas points

---
+++ src/openeo_classification/lucas.py
@@ -0,0 +1,46 @@
+import geopandas as gpd
+
+from shapely.geometry import LineString
+import pandas as pd
+import numpy as np
+import h3.api.basic_int as h3
+
+def split_lucas():
+    df = gpd.read_file("https://artifactory.vgt.vito.be/auxdata-public/openeo/2018_EU_LUCAS_POINT_110.gpkg")
+
+    cellsh3 = df.geometry.centroid.apply(lambda x: h3.geo_to_h3(x.y, x.x, 10))
+    df['s2cell'] = cellsh3
+    df.index = cellsh3
+    df.sort_index(inplace=True)
+    df = df[['POINT_ID',  's2cell',  'LC1_LABEL', 'geometry']]
+
+    def dist(x):
+        return h3.point_dist(h3.h3_to_geo(x.astype(np.int64)[-1] - 1), h3.h3_to_geo(x.astype(np.int64)[0] - 1))
+
+    distance = df.s2cell.rolling(window=2).apply(lambda x: dist(x), raw=True)
+    breakpoints = df.s2cell[distance > 200]
+
+    # out,bins = pd.qcut(df.s2cell,q=int(len(cells)/500),retbins=True)
+    cuts = breakpoints.values  # np.concatenate((breakpoints.values))
+    cuts.sort()
+    categories = pd.cut(df.index, bins=(cuts - 1), right=False)
+    grouped = df.groupby(categories)
+    count = grouped.POINT_ID.agg('count')
+    print(len(count))
+    print(len(cuts))
+    count.name = "COUNT"
+    count = count.reset_index().COUNT
+
+    union = grouped.geometry.aggregate(lambda s: s.unary_union)
+    polys = union.convex_hull
+    polys.name = "GEOM"
+
+    filenames = []
+    for name, group in grouped:
+        f = f"group_{name}.json"
+        filenames.append(f)
+        group.to_file(f)
+
+    splits_frame = gpd.GeoDataFrame({"COUNT": count, "FILENAME": pd.Series(filenames)}, geometry=polys.reset_index().GEOM)
+    splits_frame.to_file("out300.json", index=False)
+    return splits_frame
---
+++ src/openeo_classification/scripts/raw_samples_lucas.py
@@ -0,0 +1,114 @@
+
+from pathlib import Path
+
+from openeo_classification.features import creo_job_options,job_options
+from openeo_classification.lucas import split_lucas
+from openeo_classification.connection import creo,openeo,terrascope_dev
+from openeo_classification.job_management import MultiBackendJobManager
+
+import json
+import geopandas as gpd
+
+
+TIMERANGE_LUT = {
+    '2016': {'start': '2015-09-01',
+             'end': '2016-08-30'},
+    '2017': {'start': '2016-09-01',
+             'end': '2017-08-30'},
+    '2018': {'start': '2017-09-01',
+             'end': '2018-08-30'},
+    '2019': {'start': '2018-09-01',
+             'end': '2019-08-30'},
+    '2020': {'start': '2019-09-01',
+             'end': '2020-08-30'},
+    '2021': {'start': '2020-09-01',
+             'end': '2021-08-30'}
+}
+
+COLUMNS_ORDER = ['VH', 'VV', 'angle',
+                 "B01", "B02", "B03", "B04",
+                 "B05", "B06", "B07", "B08",
+                 "B09", "B11", "B12", "B8A",
+                 'SCL']
+
+def get_input_TS(eoconn, time_range, geo):
+    '''Function that will build the
+    input timeseries for the model'''
+
+    # don't consider images with more than 85% cloud coverage
+    s2_properties = {"eo:cloud_cover": lambda v: v <= 85}
+    S2_L2A = eoconn.load_collection('SENTINEL2_L2A',
+                                    bands=["B01", "B02", "B03",
+                                           "B04", "B05", "B06",
+                                           "B07", "B08", "B09",
+                                           "B11", "B12", "B8A",
+                                           "SCL"],
+                                    properties=s2_properties)
+    S2_L2A._pg.arguments['featureflags'] = {"tilesize":64}
+    S2_L2A_masked = S2_L2A.process("mask_scl_dilation", data=S2_L2A,
+                                   scl_band_name="SCL")
+    s1properties = {"polarization": lambda p: p == "DV"}
+    S1_GRD = eoconn.load_collection('SENTINEL1_GRD',
+                                    bands=['VH', 'VV'],
+                                    properties=s1properties)
+    S1_GRD._pg.arguments['featureflags'] = {"tilesize": 64}
+    S1_GRD = S1_GRD.sar_backscatter(
+        coefficient="gamma0-ellipsoid",
+        local_incidence_angle=True)
+    S1_GRD = S1_GRD.apply(lambda x: 10 * x.log(base=10))
+    S2_L2A_masked = S2_L2A_masked.resample_cube_spatial(S1_GRD)
+    merged_cube = S1_GRD.merge_cubes(S2_L2A_masked)
+
+    return merged_cube.filter_temporal(
+        time_range).aggregate_spatial(geo, reducer='mean')
+
+conn_provider = openeo
+def run(row,connection_provider,connection, provider):
+
+    fnp = row['FILENAME']
+    year = "2018"
+    time_start = TIMERANGE_LUT[year]['start']
+    time_end = TIMERANGE_LUT[year]['end']
+    time_range = [time_start, time_end]
+    pols = gpd.read_file(fnp)
+    pols["geometry"] = pols["geometry"].centroid
+    pols = pols
+    features = get_input_TS(connection,geo=json.loads(pols.to_json()),time_range=time_range)
+
+    print(f"Year: {year}; Crop ID: {fnp}")
+    job_options = {
+        "driver-memory": "4G",
+        "driver-memoryOverhead": "2G",
+        "driver-cores": "2",
+        "executor-memory": "2G",
+        "executor-memoryOverhead": "1G",
+        "executor-cores": "2",
+        "max-executors": "15",
+        "soft-errors": "true"
+    }
+
+    job = features.create_job(
+        title="Lucas {fnp}",
+        description=f"Sampling Lucas {fnp}",
+        out_format="NetCDF",
+        job_options=job_options,
+    )
+
+    job.start_job()
+    return job
+
+
+output_file = Path("sampling_lucas.csv")
+dataframe=None
+if not output_file.exists() or not output_file.is_file():
+    dataframe = split_lucas()
+
+
+manager = MultiBackendJobManager()
+manager.add_backend("terrascope", connection=terrascope_dev, parallel_jobs=1)
+
+manager.run_jobs(
+    df=dataframe,
+    start_job=run,
+    output_file=output_file
+)
